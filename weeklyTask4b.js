a53a164f7d2419355a71239cd2111ca6614bdb3b
Rahmans11 <148077278+Rahmans11@users.noreply.github.com>
2025-11-09 12:05:15 +0700

    jawaban no 4 bagian B
    penjelasan penggunaan built in method dengan tagar komentar

---
+++ weeklyTask4b.js
@@ -0,0 +1,27 @@
+// 1. method toFixed
+// method toFixed yg digunakan pada variabel yg memiliki value number decimal,
+// berguna untuk memformatkan berapa banyak angka (yg ada pada setelah ".") yg diinginkan 
+// secara spesifik, pada contoh ini kita menginginkan format number decimal yg memiliki 
+// 2 angka saja yg berada di setelah "." dengan cara memberikan angka 2 
+// pada parameter method toFixed
+
+// 2. method toString
+// method toString yg digunakan untuk mengubah suatu tipe data pada sebuah variabel menjadi 
+// bentuk / tipe data String
+
+// 3. method toPrecision
+// method toPrecision yg digunakan pada variabel yg memiliki value number decimal, berguna untuk
+// memformatkan berapa banyak total digit angka secara keseluruhan pada variabel tersebut, 
+// mau itu digit angka yg ada pada sebelum "." maupun setelahnya, pada contoh ini kita menginginkan 
+// format number decimal yg memiliki jumlah digit angkanya sebanyak 5 buah dengan cara memberikan 
+// angka 5 pada parameter method toPrecision
+
+// 4. method split
+// method split digunakan untuk mengubah tipe data string pada sebuah variabel menjadi array,
+// method ini memecah value yg bertipe data string dan memasukan setiap karakter pada value tersebut
+// kedalam index array secara satu persatu
+
+// 5. method join
+// method join yg digunakan pada contoh ini berfungsi untuk mengubah tipe data array pada 
+// variabel arrayForm menjadi string, method ini menggabungkan semua elemen array yg ditampung 
+// pada variabel tersebut menjadi satu value yg value tersebut bertipe data string
